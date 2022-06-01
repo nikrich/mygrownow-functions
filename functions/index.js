f8587715100167204ec5228ca5bd6ec8da7466a2
nikrich <jannik811@gmail.com>
2022-06-01 07:56:56 +0200

    Added eslint

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,21 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const { v4: uuidv4 } = require("uuid");
 admin.initializeApp();
 
 exports.createImage = functions.firestore
   .document("images/{imageId}")
   .onCreate((snap, context) => {
     const createdDoc = snap.data();
-    const id = uuidv4();
 
+    // Create entry item
     var entryObject = {
-      id: id,
-      uid: "todo",
+      id: createdDoc.entryId,
+      uid: createdDoc.uid,
       date: new Date(),
     };
 
-    const imageRef = admin
-      .firestore()
-      .collection("images")
-      .where("id", "==", createdDoc.id);
-
-    const doc = imageRef.get().update({ entryId: id });
-
-    console.log(doc);
-
     return admin
       .firestore()
-      .doc("entries/" + id)
+      .doc("entries/" + createdDoc.entryId)
       .set(entryObject);
   });
