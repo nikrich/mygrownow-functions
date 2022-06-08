3036bbbdfdd7297ea59593975738f0dd11c013fa
Jannik Richter <Jannik.Richter@media24.com>
2022-06-08 12:51:11 +0200

    Removed async

--- functions/index.js
+++ functions/index.js
@@ -28,7 +28,7 @@ exports.postToInstagram = functions.firestore
     const date = new Date();
     var FIVE_MIN = 5 * 60 * 1000;
     const postDate = new Date();
-    postDate.setHours(10, 0, 0);
+    postDate.setHours(13, 0, 0);
     const diff = Math.abs(date - new Date(postDate));
     console.log(diff);
 
@@ -41,6 +41,7 @@ exports.postToInstagram = functions.firestore
       username: "mygrownow",
       password: "jX2%6Xs4^*9484UR",
     });
+
     try {
       await client.login();
     } catch (ex) {
